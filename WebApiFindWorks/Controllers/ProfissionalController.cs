8c515ac619a4f49f736f8000c77ff157d69d460d
Gabrielle Gomes <110193153+joaogcastro@users.noreply.github.com>
2023-09-20 10:50:39 -0300

    Swagger funcionando

--- WebApiFindWorks/Controllers/ProfissionalController.cs
+++ WebApiFindWorks/Controllers/ProfissionalController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using WebApiFindWorks.Models;
 using WebApiFindWorks.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace WebApiFindWorks.Controllers;
 
@@ -9,41 +10,87 @@ namespace WebApiFindWorks.Controllers;
 [Route("[controller]")]
 public class ProfissionalController : ControllerBase
 {
-    private WebApiFindWorksDbContext? _context;
+    private WebApiFindWorksDbContext? _dbContext;
 
     public ProfissionalController(WebApiFindWorksDbContext context)
     {
-        _context = context;
+        _dbContext = context;
     }
 
     [HttpGet]
 [Route("listar")]
 public async Task<ActionResult<IEnumerable<Profissional>>> Listar()
-{
-    if (_context is null)
+{ 
+    /*if (_dbContext is null) 
     {
         return NotFound();
-    }
-    return await _context.Profissional.ToListAsync();
+    }*/
+    return await _dbContext.Profissional.ToListAsync();
 }
 
     [HttpGet()]
     [Route("buscar/{nome}")]
     public async Task<ActionResult<Profissional>> Buscar([FromRoute] string nome)
     {
-        if(_context.Profissional is null)
+        if(_dbContext.Profissional is null)
             return NotFound();
-        var profissional = await _context.Profissional.FindAsync(nome);
+        var profissional = await _dbContext.Profissional.FindAsync(nome);
         if (profissional is null)
             return NotFound();
         return profissional;
     }
     [HttpPost]
     [Route("cadastrar")]
-    public IActionResult Cadastrar(Profissional profissional)
+    public async  Task<IActionResult> Cadastrar(Profissional profissional)
     {
-        _context.Add(profissional);
-        _context.SaveChanges();
+
+        if(_dbContext.Profissional is null) return NotFound();
+        _dbContext.AddAsync(profissional);
+        await _dbContext.SaveChangesAsync();
         return Created("", profissional);
     }
+
+
+    [HttpPut()]
+    [Route("alterar")]
+
+    public async Task<ActionResult> Alterar(Profissional profissional)
+    {
+
+        if(_dbContext.Profissional is null) return NotFound();
+        if(await _dbContext.Profissional.FindAsync(profissional) is null)
+            return NotFound();
+        _dbContext.Update(profissional);
+        await _dbContext.SaveChangesAsync();
+        return Ok();
+
+    }
+
+    [HttpPatch()]
+    [Route("mudardescricao/{nome}")]
+    public async Task<ActionResult> MudarDescricao(string nome)
+    {
+        if(_dbContext.Profissional is null) return NotFound();
+        var profissionalTemp = await _dbContext.Profissional.FindAsync(nome);
+        if(profissionalTemp is null) return NotFound();
+        profissionalTemp.Nome = nome;
+        await _dbContext.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpPatch()]
+    [Route("excluir")]
+    public async Task<ActionResult> Excluir(string nome)
+    {
+        if(_dbContext.Profissional is null) return NotFound();
+        var profissional = await _dbContext.Profissional.FindAsync(nome);
+        if(profissional is null) return NotFound();
+        _dbContext.Profissional.Remove(profissional);
+        await _dbContext.SaveChangesAsync();
+        return Ok();
+
+    }
+
+
+
 }
