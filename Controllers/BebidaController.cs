fd538246311e5d507ab0fa38c4e87e5246297595
Joao Castro <110193153+joaogcastro@users.noreply.github.com>
2023-10-07 16:20:57 -0300

    Updated BebidaController.cs

--- Controllers/BebidaController.cs
+++ Controllers/BebidaController.cs
@@ -36,4 +36,60 @@ public class BebidaController : ControllerBase
         await _dbContext.SaveChangesAsync();
         return Created("", Bebida);
     }
+
+    [HttpGet()] 
+     [Route("buscar/{id}")] 
+     public async Task<ActionResult<Cliente>> Buscar([FromRoute] int id) 
+     { 
+         if (_dbContext is null) 
+             return NotFound(); 
+         var bebida= await _dbContext.Cliente.FindAsync(id); 
+         if (bebidais null) 
+             return NotFound(); 
+         return cliente; 
+     } 
+  
+     [HttpPut()] 
+     [Route("alterar")] 
+     public async Task<ActionResult> Alterar(Bebidacliente) 
+     { 
+         if (_dbContext is null) 
+             return NotFound(); 
+  
+         // Busque o registro existente pelo ID (ou outra chave primária) do usuário 
+         var existingBebida= await _dbContext.Cliente.FindAsync(cliente.Id); 
+  
+         if (existingBebidais null) 
+             return NotFound(); 
+  
+         // Atualize apenas os campos que foram fornecidos no objeto usuário 
+         if (cliente.NomeBebida!= "string") 
+         { 
+             existingCliente.NomeBebida= cliente.NomeCliente; 
+         } 
+  
+         if (cliente.Senha != "string") 
+         { 
+             existingCliente.Senha = cliente.Senha; 
+         } 
+  
+         // Marque o registro como modificado no contexto do EF 
+         _dbContext.Entry(existingCliente).State = EntityState.Modified; 
+  
+         // Salve as alterações no banco de dados 
+         await _dbContext.SaveChangesAsync(); 
+  
+         return Ok(); 
+     } 
+  
+     [HttpDelete()] 
+     [Route("excluir/{id}")] 
+     public async Task<ActionResult> Excluir([FromRoute] int id) 
+     { 
+         if (_dbContext is null) return NotFound(); 
+         var bebida= await _dbContext.Cliente.FindAsync(id); 
+         if (bebidais null) return NotFound(); 
+         _dbContext.Cliente.Remove(cliente); 
+         await _dbContext.SaveChangesAsync(); 
+         return Ok(); 
 }
